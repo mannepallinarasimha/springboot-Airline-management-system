f5335d09b04d3a31385709ed07f6a054848e5be5
manne <mannepallinarasimha@gmail.com>
2026-04-22 22:03:47 +0530

    airport controller updated v1

--- microservices/services/location-service/src/main/java/com/nara/controller/AirportController.java
+++ microservices/services/location-service/src/main/java/com/nara/controller/AirportController.java
@@ -2,6 +2,7 @@ package com.nara.controller;
 
 import com.nara.payload.request.AirportRequest;
 import com.nara.payload.response.AirportResponse;
+import com.nara.payload.response.ApiResponse;
 import com.nara.service.AirportService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -26,13 +27,29 @@ public class AirportController {
     }
 
     @GetMapping(path = "/{id}")
-    public ResponseEntity<AirportResponse> getAirportById(@PathVariable("id") Long id) throws Exception {
+    public ResponseEntity<AirportResponse> getAirportById(@PathVariable Long id) throws Exception {
         return ResponseEntity.ok(airportService.getAirportById(id));
     }
 
-//    @GetMapping(path = "city/{cityId}")
-//    public ResponseEntity<List<AirportResponse>> getAllAirports(@PathVariable("cityId") Long cityId) throws Exception {
-//        return ResponseEntity.ok(airportService.getAirportById(id));
-//    }
+    @GetMapping
+    public ResponseEntity<List<AirportResponse>> getAllAirports() throws Exception {
+        return ResponseEntity.ok(airportService.getAllAirports());
+    }
+
+    @GetMapping(path="city/{cityId}")
+    public ResponseEntity<List<AirportResponse>> getAirportsByCityId(@PathVariable Long cityId) throws Exception {
+        return ResponseEntity.ok(airportService.getAirportByCityId(cityId));
+    }
+
+    @PutMapping(path="/{id}")
+    public ResponseEntity<AirportResponse> updateAirport(@PathVariable Long id, @RequestBody AirportRequest airportRequest) throws Exception {
+        return ResponseEntity.ok(airportService.updateAirport(id, airportRequest));
+    }
+
+    @DeleteMapping(path="/{id}")
+    public ResponseEntity<ApiResponse> deleteAirport(@PathVariable Long id) throws Exception {
+        airportService.deleteAirport(id);
+        return ResponseEntity.ok(new ApiResponse("Deleted Successfully"));
+    }
     
 }

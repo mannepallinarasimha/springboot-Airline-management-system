20b19d858bdb32aeb08b11261992a02e767de863
manne <mannepallinarasimha@gmail.com>
2026-04-24 19:06:32 +0530

    flight-schedule files added v2.

--- microservices/services/flight-ops-service/src/main/java/com/nara/flight_ops_service/service/impl/FlightScheduleServiceImpl.java
+++ microservices/services/flight-ops-service/src/main/java/com/nara/flight_ops_service/service/impl/FlightScheduleServiceImpl.java
@@ -1,22 +1,37 @@
 package com.nara.flight_ops_service.service.impl;
 
+import com.nara.enums.FlightStatus;
+import com.nara.flight_ops_service.mapper.FlightScheduleMapper;
 import com.nara.flight_ops_service.model.Flight;
+import com.nara.flight_ops_service.model.FlightSchedule;
 import com.nara.flight_ops_service.repository.FlightRepository;
+import com.nara.flight_ops_service.repository.FlightScheduleRepository;
+import com.nara.flight_ops_service.service.FlightInstanceService;
 import com.nara.flight_ops_service.service.FlightScheduleService;
+import com.nara.payload.request.FlightInstanceRequest;
 import com.nara.payload.request.FlightScheduleRequest;
+import com.nara.payload.response.AirlineResponse;
+import com.nara.payload.response.AirportResponse;
 import com.nara.payload.response.FlightScheduleResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class FlightScheduleServiceImpl implements FlightScheduleService {
     private final FlightRepository flightRepository;
+    private final FlightScheduleRepository flightScheduleRepository;
+    private final FlightInstanceService flightInstanceService;
 
     @Override
-    public FlightScheduleResponse createFlightSchedule(Long userId, FlightScheduleRequest request) throws Exception {
+    public FlightScheduleResponse createFlightSchedule(Long airlineId, FlightScheduleRequest request) throws Exception {
+
+        // todo - watch for airlineId
         Flight flight = flightRepository
                 .findById(request.getFlightId()).orElseThrow(() -> new Exception("flight not found with id"));
 
@@ -24,12 +39,35 @@ public class FlightScheduleServiceImpl implements FlightScheduleService {
         if(request.getEndDate().isBefore(request.getStartDate())){
             throw new Exception("end date is before start date");
         }
+        FlightSchedule savedSchedule = FlightScheduleMapper.toEntity(request, flight);
 
-        return null;
+        //we need to create flights from start date to enddate
+        List<DayOfWeek> operationDays = savedSchedule.getOperatingDays();
+        LocalDate startDate = savedSchedule.getStartDate();
+        LocalDate endDate = savedSchedule.getEndDate();
+
+        FlightInstanceRequest flightInstanceRequest = FlightInstanceRequest.builder()
+                .scheduledId(savedSchedule.getId())
+                .flightId(flight.getId())
+                .arrivalAirportId(flight.getArrivalAirportId())
+                .departureAirportId(flight.getDepartureAirportId())
+                .status(FlightStatus.SCHEDULED)
+                .build();
+
+        for(LocalDate date = startDate; !date.isAfter(endDate); date=date.plusDays(1)){
+            if(operationDays.contains(date.getDayOfWeek())){
+                flightInstanceRequest.setDepartureDateTime(LocalDateTime.of(date, savedSchedule.getDepartureTime()));
+                flightInstanceRequest.setArrivalDateTime(LocalDateTime.of(date, savedSchedule.getArrivalTime()));
+                flightInstanceService.createFlightInstance(airlineId, flightInstanceRequest);
+            }
+
+        }
+        return convertToFlightSchedule(savedSchedule);
     }
 
     @Override
     public FlightScheduleResponse getFlightScheduleById(Long id) {
+
         return null;
     }
 
@@ -46,5 +84,14 @@ public class FlightScheduleServiceImpl implements FlightScheduleService {
     @Override
     public void deleteFlightSchedule(Long id) {
 
+    }
+
+    private FlightScheduleResponse convertToFlightSchedule(FlightSchedule flightSchedule){
+
+        // todo - service to service communication
+        AirportResponse departureAirport = AirportResponse.builder().id(flightSchedule.getDepartureAirportId()).build();
+        AirportResponse arrivalAirport = AirportResponse.builder().id(flightSchedule.getArrivalAirportId()).build();
+
+        return FlightScheduleMapper.toResponse(flightSchedule, arrivalAirport, departureAirport);
     }
 }
